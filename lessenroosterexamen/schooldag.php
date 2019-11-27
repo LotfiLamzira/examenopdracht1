61cefbfff1485d62d11a84f3142e2da029dbb090
Ahmed202022 <56867990+Ahmed202022@users.noreply.github.com>
2019-11-27 12:08:10 +0100

    Add files via upload

---
+++ lessenroosterexamen/schooldag.php
@@ -0,0 +1,17 @@
+<?php
+	class	Test
+	{
+		private	$_weekDag	=	"";
+		private	$_start	=	0;
+		private	$_einde	=	0;
+		
+		public function __construct($weekDag,	$start,	$einde) 
+		{
+			$this->_weekDag	= $weekDag;
+			$this->_start = $start;
+    	$this->Datum  = $einde;
+
+		}
+		
+		
+	}
